39dfe1060b77f2143f67a676da862cb39df3d7e8
beva3 <mizabienvenu@gmail.com>
2024-11-13 22:12:32 +0530

    prototype

---
+++ todoList.py
@@ -0,0 +1,13 @@
+class Todo_list:
+    def __init__(self):
+        pass
+    def add_task(self, task):
+        pass
+    def view_task(self, tasks):
+        pass
+    def remove_task(self, task):
+        pass
+    
+
+# Example usage
+todo_list = Todo_list()
