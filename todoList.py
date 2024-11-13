4c7b7ae4cd53e09076e3d2f72be58a7f82b38654
beva3 <mizabienvenu@gmail.com>
2024-11-13 22:49:08 +0530

    todolist

--- todoList.py
+++ todoList.py
@@ -1,3 +1,15 @@
+# class Task:
+#     def __init__(self,name,is_completed=False):
+#         print('init task')
+#         self.name = name
+#         self.is_completed = is_completed
+#     def mark_completed(self):
+#         self.is_completed = True
+#         print(f'{self.name} marked as completed')
+#     def __str__(self):
+#         status = 'completed' if self.is_completed else 'Pending'
+#         return f'{self.name} - {status}'
+    
 class Todo_list:
     def __init__(self):
         print('init todo list')
@@ -17,8 +29,9 @@ class Todo_list:
         for task in tasks:
             print(task)
 
-    def remove_task(self, task):
-        print('remove task')
+    def remove_task(self, task_name):
+        print(f'remove task : {task_name}')
+       
     
 
 # Example usage
@@ -28,3 +41,6 @@ todo_list.add_task('Task 2')
 
 # view_task
 todo_list.view_task(todo_list.tasks)
+
+# remove_task
+todo_list.remove_task('Task 1')
