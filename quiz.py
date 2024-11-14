d168f19632c77b0284de108a06013a1633a8c2e7
beva3 <mizabienvenu@gmail.com>
2024-11-14 16:46:48 +0530

    prototype

---
+++ quiz.py
@@ -0,0 +1,11 @@
+class Question:
+    def __init__(self,text,options,correct_answer=False):
+        pass
+    def is_correct(self,user_answer):
+        pass
+    
+class Quiz:
+    def __init__(self,Question):
+        pass
+    def add_quiz(self,question):
+        pass
